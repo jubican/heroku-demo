24a3523b6b086ac8825a1a1b2374759bcb6eee23
jubican <jubican@gmail.com>
2017-10-06 18:15:11 -0700

    Build application interface

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
-<?
-echo "<html><body>This is a test</body></html>"
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+<link rel="stylesheet" type="text/css" href="assets/fonts/stylesheet.css" />
+</head>
+<body>
+    <div class="header">
+        <div class="heading">AWS + Salesforce Heroku Demo</div>
+    </div>
+    <div class="main_body">
+        <div class="body_content">
+            <div class="table">
+                <div class="table_header">
+                    <div class="table_cell company_field">Company</div>
+                    <div class="table_cell customer_field">Customer</div>
+                    <div class="table_cell counter_field">Counter</div>
+                    <div class="table_cell action_field">Action</div>
+                    <div class="table_cell_end"></div>
+                </div>
+                <div class="table_row">
+                    <div class="table_cell company_field">AbracadabraAbracadabraAbracadabraAbracadabraAbracadabraAbracadabraAbracadabraAbracadabraAbracadabra</div>
+                    <div class="table_cell customer_field">Kevin Cochran</div>
+                    <div class="table_cell counter_field">16</div>
+                    <div class="table_cell action_field">
+                        <button class="action_button">Make my day</button>
+                    </div>
+                    <div class="table_cell_end"></div>
+                </div>
+                <div class="table_row">
+                    <div class="table_cell company_field">Abracadabra</div>
+                    <div class="table_cell customer_field">Kevin Cochran</div>
+                    <div class="table_cell counter_field">16</div>
+                    <div class="table_cell action_field">
+                        <button class="action_button">Make my day</button>
+                    </div>
+                    <div class="table_cell_end"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
