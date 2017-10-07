98397573fa856c1f47807c45b199e4822ca461f4
jubican <jubican@gmail.com>
2017-10-06 19:12:31 -0700

    add database read

--- index.php
+++ index.php
@@ -13,29 +13,31 @@
             <div class="table">
                 <div class="table_header">
                     <div class="table_cell company_field">Company</div>
-                    <div class="table_cell customer_field">Customer</div>
+                    <div class="table_cell customer_field">Description</div>
                     <div class="table_cell counter_field">Counter</div>
                     <div class="table_cell action_field">Action</div>
                     <div class="table_cell_end"></div>
                 </div>
+<?php
+$db = pg_connect("host=ec2-54-163-230-219.compute-1.amazonaws.com port=5432 dbname=d4qutjb0l5o7tq user=rycnwatsutqwyy password=908a4fa5727438207db3a33386a26b0b40f0d0504b5c34bb256f8d6885141acd");
+
+$rs = pg_query($db, "select sfid, name, counter__c, description from salesforce.account order by name");
+while ($row = pg_fetch_row($rs)) {
+    ?>
                 <div class="table_row">
-                    <div class="table_cell company_field">AbracadabraAbracadabraAbracadabraAbracadabraAbracadabraAbracadabraAbracadabraAbracadabraAbracadabra</div>
-                    <div class="table_cell customer_field">Kevin Cochran</div>
-                    <div class="table_cell counter_field">16</div>
+                    <div class="table_cell company_field"><?php echo $row[1] ?></div>
+                    <div class="table_cell customer_field" label="<?php echo $row[3] ?>"><?php echo $row[3] ?></div>
+                    <div class="table_cell counter_field"><?php echo $row[2] ?></div>
                     <div class="table_cell action_field">
-                        <button class="action_button">Make my day</button>
+                        <button class="action_button">Subscribe</button>
                     </div>
                     <div class="table_cell_end"></div>
                 </div>
-                <div class="table_row">
-                    <div class="table_cell company_field">Abracadabra</div>
-                    <div class="table_cell customer_field">Kevin Cochran</div>
-                    <div class="table_cell counter_field">16</div>
-                    <div class="table_cell action_field">
-                        <button class="action_button">Make my day</button>
-                    </div>
-                    <div class="table_cell_end"></div>
-                </div>
+    <?php
+}
+
+pg_close($db);
+?>
             </div>
         </div>
     </div>
